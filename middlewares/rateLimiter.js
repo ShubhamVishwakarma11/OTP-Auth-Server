fbd99ff0306d165b374eba137a92610d73c57512
Shubham Vishwakarma <shubham.vish.2633@gmail.com>
2023-06-07 19:07:53 +0530

    fix: extract ip from req.headers

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,10 +2,10 @@ const Limiter = require('../models/limiterModel');
 
 const rateLimiter = ({allowedMinutes, allowedHits}) => {
     return async (req,res,next) => {
-        // const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        const ip = req.ip;
+        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        // const ip = req.ip;
 
-        console.log("ip: ", ip);
+        console.log("req: ", req);
 
         const data = await Limiter.findOne({ip});
 
